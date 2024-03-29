4917d59c80e755587b3a63a305729698b0d5f6aa
Brendan McBryan <brendan.mcbryan@gmail.com>
2024-03-29 09:05:34 -0700

    styled-components implemntation

--- src/App.jsx
+++ src/App.jsx
@@ -1,15 +1,43 @@
 import styled from 'styled-components';
+import GlobalStyles from './styles/GlobalStyles';
+
+import Button from './ui/Button';
+import Input from './ui/Input';
 
 const H1 = styled.h1`
   font-size: 30px;
   font-weight: 600;
+  background-color: cyan;
+`;
+
+const StyleApp = styled.main`
+  background-color: orange;
+  padding: 20px;
 `;
 
 function App() {
   return (
-    <div>
-      <H1>The Wild Oasis</H1>
-    </div>
+    <>
+      <GlobalStyles />
+      <StyleApp>
+        <H1>The Wild Oasis</H1>
+        <Button
+          onClick={() => {
+            alert('check in ding');
+          }}
+        >
+          check in
+        </Button>
+        <Button
+          onClick={() => {
+            alert('check out ding');
+          }}
+        >
+          check out
+        </Button>
+        <Input type="number" placeholder="Number of Guests" />
+      </StyleApp>
+    </>
   );
 }
 
