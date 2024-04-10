0a4490f87bc673662d45f631bdd3614d9e9c991f
Brendan McBryan <brendan.mcbryan@gmail.com>
2024-04-10 13:29:50 -0700

    duration summary implemented

--- src/features/dashboard/DashboardLayout.jsx
+++ src/features/dashboard/DashboardLayout.jsx
@@ -6,6 +6,7 @@ import Spinner from '../../ui/Spinner';
 import Stats from './Stats';
 import { useCabins } from '../cabins/useCabins';
 import SalesChart from './SalesChart';
+import DurationChart from './DurationChart';
 
 const StyledDashboardLayout = styled.div`
   display: grid;
@@ -40,7 +41,7 @@ function DashboardLayout() {
       />
 
       <div>Activities</div>
-      <div>Chart for duration</div>
+      <DurationChart confirmedStays={confirmedStays} />
       <SalesChart bookings={bookings} numDays={numDays} />
     </StyledDashboardLayout>
   );
