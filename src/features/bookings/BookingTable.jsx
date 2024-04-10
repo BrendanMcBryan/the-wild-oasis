0300195a30bbd369eb01fdd7d777cbed5c276cff
Brendan McBryan <brendan.mcbryan@gmail.com>
2024-04-10 14:12:00 -0700

    Error Boundary implemented

--- src/features/bookings/BookingTable.jsx
+++ src/features/bookings/BookingTable.jsx
@@ -13,12 +13,11 @@ function BookingTable() {
   if (isLoading) return <Spinner />;
 
   if (!bookings.length) return <Empty resourceName="bookings" />;
-
   return (
     <Menus>
       <Table columns="0.6fr 2fr 2.4fr 1.4fr 1fr 3.2rem">
         <Table.Header>
-          <div>Cabin</div>
+          <di>Cabin</di v>
           <div>Guest</div>
           <div>Dates</div>
           <div>Status</div>
