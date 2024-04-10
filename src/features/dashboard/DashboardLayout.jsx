5fa23e2f3aae882293ec2e86bb0c5dc50fe1c136
Brendan McBryan <brendan.mcbryan@gmail.com>
2024-04-10 11:04:32 -0700

    Dashboard calculations

--- src/features/dashboard/DashboardLayout.jsx
+++ src/features/dashboard/DashboardLayout.jsx
@@ -1,4 +1,10 @@
-import styled from "styled-components";
+/* eslint-disable no-unused-vars */
+import styled from 'styled-components';
+import { useRecentBookings } from './useRecentBookings';
+import { useRecentStays } from './useRecentStays';
+import Spinner from '../../ui/Spinner';
+import Stats from './Stats';
+import { useCabins } from '../cabins/useCabins';
 
 const StyledDashboardLayout = styled.div`
   display: grid;
@@ -6,3 +12,37 @@ const StyledDashboardLayout = styled.div`
   grid-template-rows: auto 34rem auto;
   gap: 2.4rem;
 `;
+
+function DashboardLayout() {
+  const { bookings, isLoading: isLoadingBookings } = useRecentBookings();
+  const {
+    stays,
+    confirmedStays,
+    isLoading: isLoadingStays,
+    numDays,
+  } = useRecentStays();
+
+  const { cabins, isLoading: isLoadingCabins } = useCabins();
+
+  if (isLoadingBookings || isLoadingStays || isLoadingCabins)
+    return <Spinner />;
+
+  // console.log(stays);
+
+  return (
+    <StyledDashboardLayout>
+      <Stats
+        bookings={bookings}
+        confirmedStays={confirmedStays}
+        numDays={numDays}
+        cabinCount={cabins.length}
+      />
+
+      <div>Activities</div>
+      <div>Chart for duration</div>
+      <div>Chart for sales</div>
+    </StyledDashboardLayout>
+  );
+}
+
+export default DashboardLayout;
